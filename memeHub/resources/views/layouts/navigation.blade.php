940393aeff80664a170fcec109d5be6eb01bb353
Bosni4ko <stefanbosnyak9@gmail.com>
2021-06-16 12:28:03 +0300

    Navbar_design_v1

--- memeHub/resources/views/layouts/navigation.blade.php
+++ memeHub/resources/views/layouts/navigation.blade.php
@@ -1,19 +1,18 @@
 
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 sm:pb-10">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl px-4 sm:px-6 mx-auto lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
+            <!-- Logo -->
+                <div class="flex-shrink-0 flex items-left">
                     <a href="{{ route('meme.index') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
-
+            <div class="flex">            
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('meme.index')" :active="request()->routeIs('meme.index')">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:mt-10 sm:flex">
+                    <x-nav-link :href="route('meme.index')" :active="request()->routeIs('meme.index') ">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('meme.index')" :active="request()->routeIs('meme.index')">
@@ -25,11 +24,13 @@
                     <x-nav-link :href="route('meme.index')" :active="request()->routeIs('meme.index')">
                         {{ __('Create') }}
                     </x-nav-link>
-                    <x-input type="text" name="search" required/>
-                    <x-button type="submit">Search</x-button>
                     
                     
                 </div>
+            </div>
+            <div class="flex mt-9">
+                    <x-input  type="text" name="search" required/>
+                    <x-button type="submit">Search</x-button>
             </div>
 
             <!-- Settings Dropdown -->
@@ -37,8 +38,8 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
+                        <button class="flex mt-8 bg-gray-800 border border-transparent rounded-md font-semibold inline-flex items-center px-4 py-2 text-white">
+                            <div style="font-size:16px;">{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -48,12 +49,12 @@
                         </button>
                     </x-slot>
 
-                    <x-slot name="content">
+                    <x-slot name="content" >
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
+                            <x-dropdown-link :href="route('logout') "
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
@@ -64,8 +65,9 @@
             </div>
             @endif
             @if(Auth::guest())
-            <x-nav-link :href="route('login')" :active="request()->routeIs('Login')">{{__('Log in')}}</x-nav-link>
-            <x-nav-link :href="route('login')" :active="request()->routeIs('Register')">{{__('Register')}}</x-nav-link>
+            <div class="flex mt-8 bg-gray-800 border border-transparent rounded-md font-semibold inline-flex items-center px-4 py-2">            
+                <x-nav-link :href="route('login')" :active="request()->routeIs('Login')" class="text-white">{{__('Log in')}}</x-nav-link>         
+            </div>
             @endif
 
             <!-- Hamburger -->
@@ -79,9 +81,8 @@
             </div>
         </div>
     </div>
-
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden flex">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('meme.index')" :active="request()->routeIs('meme.index')">
                 {{ __('Dashboard') }}
@@ -90,7 +91,7 @@
 
         <!-- Responsive Settings Options -->
         @if(Auth::check())
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div >
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
