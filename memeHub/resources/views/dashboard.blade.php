53eab6bbe481134b17577bd591a60b973275d320
Bosni4ko <stefanbosnyak9@gmail.com>
2021-06-18 15:44:26 +0300

    Upload

--- memeHub/resources/views/dashboard.blade.php
+++ memeHub/resources/views/dashboard.blade.php
@@ -1,26 +1,12 @@
 
 <x-app-layout>
-<x-slot name="header">
+<x-slot name="header">  
 </x-slot>
 <div class='container'>
     <div class='row justify-content-center'>
         <div class='col-md-10'>
             <div class="card">
-                <div class="card-header">
-                    <h4>Upload meme</h4>
-                    @if(Auth::check())
-                    <form action='/meme' enctype="multipart/form-data" method="POST" class='form-inline'>
-                        @csrf
-                        <div class='form-group'>
-                            <input type='file' name='meme' class="form-control-image">
-                        </div>
-                        <input type='submit' value="Upload" class="btn btn-primary">
-                    </form>
-                    @endif
-                    @if(Auth::guest())
-                    Meme list
-                    @endif
-                </div>
+               
                 <div class='card-body'>
                     <div class='container mt-2'>
                         <div class='row justify-content-center'>
--- memeHub/resources/views/upload.blade.php
+++ memeHub/resources/views/upload.blade.php
@@ -8,9 +8,21 @@
 <div class='container'>
     <div class='row justify-content-center'>
         <div class='col-md-10'>
-            <div class="card">
-            
-            </div>
+            <div class="card-header">
+                    <h4>Upload meme</h4>
+                    @if(Auth::check())
+                    <form action='/meme' enctype="multipart/form-data" method="POST" class='form-inline'>
+                        @csrf
+                        <div class='form-group'>
+                            <input type='file' name='meme' class="form-control-image">
+                        </div>
+                        <input type='submit' value="Upload" class="btn btn-primary">
+                    </form>
+                    @endif
+                    @if(Auth::guest())
+                    Meme list
+                    @endif
+                </div>
         </div>
     </div>
 </div>
