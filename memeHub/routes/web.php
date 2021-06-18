1194158eb0c6a8995e8a6e6f9268b7008d3cc81c
eddyChiks <85617801+eddyChiks@users.noreply.github.com>
2021-06-18 13:30:42 +0300

    likes

--- memeHub/routes/web.php
+++ memeHub/routes/web.php
@@ -18,7 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('meme', MemeController::class);
 Route::get('/',[MemeController::class,'index']);
+Route::get('/dashboard',[MemeController::class,'index'])->middleware(['auth'])->name('dashboard');
 require __DIR__.'/auth.php';
+
 
 Route::post('/meme',[MemeController::class,'upload']);
 Route::delete('/meme/{id}',[MemeController::class,'destroy']);
+Route::post('meme/like',[MemeController::class,'like'])->name("meme.like");
+Route::post('meme/dislike',[MemeController::class,'dislike'])->name("meme.dislike");
