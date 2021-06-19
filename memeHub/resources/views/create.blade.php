307ac89f839b4a524e01956027ad3353ae25d5e2
Vuaer <mihail.avlasenko@gmail.com>
2021-06-19 13:46:51 +0300

    function and templates create

--- memeHub/resources/views/create.blade.php
+++ memeHub/resources/views/create.blade.php
@@ -9,9 +9,25 @@
     <div class='row justify-content-center'>
         <div class='col-md-10'>
             <div class="card">
-            
+                        @if (true)
+                        <p> I have one record!</p>
+                                @pastetemplates(); 
+                        @endif
+
             </div>
         </div>
     </div>
 </div>
 </x-app-layout>
+<?php
+
+function pastetemplates() {
+$files = glob("memes/templates/*.*");
+for ($i = 0; $i < count($files); $i++) {
+    $image = $files[$i];
+    echo basename($image) . "<br />"; // show only image name if you want to show full path then use this code // echo $image."<br />";
+    echo '<img src="' . $image . '" alt="Random image" />' . "<br /><br />";
+
+}
+}
+?>
