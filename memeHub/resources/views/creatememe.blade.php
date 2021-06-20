7a9c39f6d64e1f2eb6d40d53f1af33edc596585a
Vuaer <mihail.avlasenko@gmail.com>
2021-06-20 18:09:04 +0300

    i dont know what to do

--- memeHub/resources/views/creatememe.blade.php
+++ memeHub/resources/views/creatememe.blade.php
@@ -9,13 +9,35 @@
     <div class='row justify-content-center'>
         <div class='col-md-10'>
             <div class="card">
+                   
                       <?php    
+                        
                         echo '<p>';
                         echo ''. $id .'';
-                        echo '</p>';
-                        findtemplate($id);
-                       ?>
-                   
+                        echo '</p>';                        
+                        ?>
+                     <form action="foobar_submit.php" method="get">
+                           <input type="text" name="subject" id="subject" value="your text">
+                           <input type="submit" name="my_form_submit_button" 
+                                  value="add you text"/>
+
+                       </form>
+                       <?php    
+                        
+                        echo '<p>';
+                        postresult($id);
+                        echo '</p>';                        
+                        ?>
+                    @if(Auth::check())
+                        @csrf
+                        <button>
+                            
+<!--                        <button  action='/meme'  type='file' name='meme' class="form-control-image" value="{$img}" >
+                           press me
+                        <button/>-->
+
+                    @endif                        
+                    
             </div>
         </div>
     </div>
@@ -27,7 +49,9 @@ function findtemplate($id) {
 $files = glob("memes/templates/*.*");
 
 for ($i = 0; $i < count($files); $i++) {
-    $image = $files[$i];
+    
+    global $image;
+    $image= $files[$i];
     //echo basename($image) . "<br />"; // show only image name if you want to show full path then use this code // echo $image."<br />";
     if(basename($image)==$id)
     {
@@ -40,5 +64,29 @@ for ($i = 0; $i < count($files); $i++) {
  echo '</div>';
  echo '<br />';
   
-}                                          
+}              
+function postresult($id)
+{
+    findtemplate($id);
+                        // (A) OPEN IMAGE
+                        $imgPath = 'memes/templates/'.$id.'';
+                       
+                        global $img;
+                        $img = imagecreatefromjpeg($imgPath);
+
+                        // (B) WRITE TEXT
+                        $white = imagecolorallocate($img, 0xFF, 0xFF, 0xFF);
+                        $txt = "in funcion";
+                        $font = "C:\Windows\Fonts\arial.ttf"; 
+                        imagettftext($img, 24, 0, 5, 24, $white, $font, $txt);
+
+                        // (C) OUTPUT IMAGE
+                     header('Content-Type: image/jpeg');
+                     
+//                         imagedestroy($img);
+//
+//                        // OR SAVE TO A FILE
+                        // THE LAST PARAMETER IS THE QUALITY FROM 0 to 100
+                        imagejpeg($img, "18_06.jpg", 100); 
+}
 ?>
