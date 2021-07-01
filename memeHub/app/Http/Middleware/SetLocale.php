a6086a820257bfbc0693b372fa1a029c5ee1af29
Vuaer <mihail.avlasenko@gmail.com>
2021-07-01 12:43:51 +0300

    Update SetLocale.php

--- memeHub/app/Http/Middleware/SetLocale.php
+++ memeHub/app/Http/Middleware/SetLocale.php
@@ -29,9 +29,21 @@ class SetLocale
 
         if ($request->session()->has('locale')) {
             $locale = $request->session()->get('locale');
+            config(['app.locale' => $locale]);
         }
-        
-        config(['app.locale' => $locale]);
+        else {
+            $availableLangs = ['lv', 'en'];
+            $userLangs = preg_split('/,|;/', $request->server('HTTP_ACCEPT_LANGUAGE'));
+
+            foreach ($availableLangs as $lang) {
+                if(in_array($lang, $userLangs)) {
+                    App::setLocale($lang);
+                    session('locale', $lang);
+                    break;
+                }
+            }
+        }
+        //config(['app.locale' => $locale]);
         //App::setLocale(session($locale));
         
         return $next($request);
